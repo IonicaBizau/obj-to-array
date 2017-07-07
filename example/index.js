b9cc448cdabf749bf4dd0a48a75fed518c2cb7ea
Ionică Bizău <bizauionica@gmail.com>
2017-07-07 14:32:26 +0300

    Initial

--- example/index.js
+++ example/index.js
@@ -2,4 +2,29 @@
 
 const objToArray = require("../lib");
 
-console.log(objToArray());
+const categories = {
+    "planets": {
+        posts: [
+            "Hello Pluto",
+            "Messages from Earth"
+        ]
+    },
+    "dummy": {
+        posts: [
+            "Foo",
+            "Bar"
+        ]
+    }
+};
+
+console.log(objToArray(categories));
+// =>
+// [ { posts: [ 'Hello Pluto', 'Messages from Earth' ] },
+//   { posts: [ 'Foo', 'Bar' ] } ]
+
+// Append the category field as well
+console.log(objToArray(categories, "category"));
+// =>
+// [ { posts: [ 'Hello Pluto', 'Messages from Earth' ],
+//     category: 'planets' },
+//   { posts: [ 'Foo', 'Bar' ], category: 'dummy' } ]
